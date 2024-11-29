c77ab7c2639cabd321d48f0842ea040dd35d1679
Hung <skellybg245@gmail.com>
2024-11-29 23:53:34 +1100

    feat(struct): add ray (sus)

--- src/structs/color.rs
+++ src/structs/color.rs
@@ -1,16 +1,14 @@
-use super::vec3::Vec3;
-
-pub struct Color(Vec3);
+pub struct Color((f64, f64, f64));
 
 impl Color {
     pub fn new(r: f64, g: f64, b: f64) -> Self {
-        Self(Vec3::from((r, g, b)))
+        Self((r, g, b))
     }
 
     pub fn write_color(&self) {
-        let r = self.0.x;
-        let g = self.0.y;
-        let b = self.0.z;
+        let r = self.0 .0;
+        let g = self.0 .1;
+        let b = self.0 .2;
 
         let ir = (255.999 * r).floor() as i32;
         let ig = (255.999 * g).floor() as i32;
--- src/structs/mod.rs
+++ src/structs/mod.rs
@@ -1,3 +1,4 @@
-mod vec3;
-
 pub mod color;
+mod point3;
+pub mod ray;
+mod vec3;
