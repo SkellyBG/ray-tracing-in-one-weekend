b97b7eaa8e7a020be7ab2eb67b6335b564d01e63
Hung <skellybg245@gmail.com>
2024-11-29 21:10:00 +1100

    feat: add log

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ const IMAGE_HEIGHT: usize = 256;
 fn main() {
     print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
     for j in 0..IMAGE_HEIGHT {
+        eprintln!("\rScanlines remaining: {} ", IMAGE_HEIGHT - j);
         for i in 0..IMAGE_WIDTH {
             let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
             let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
@@ -16,4 +17,6 @@ fn main() {
             println!("{} {} {}", ir, ig, ib);
         }
     }
+
+    eprintln!("\rDone.")
 }
